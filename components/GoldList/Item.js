dc1e689bb80cbe3f22ad193fe08e5e15deb420d3
marklin2012 <marklin2012@sina.cn>
2020-01-03 01:33:45 +0800

    feat: 调整掘金界面样式

--- components/GoldList/Item.js
+++ components/GoldList/Item.js
@@ -3,14 +3,18 @@ import PropType from 'prop-types'
 import moment from 'moment'
 import Link from 'next/link'
 import styles from './Item.module.less'
+import { Icon } from 'antd'
 
 const Item = ({ item }) => {
   return <Link href={item.url} prefetch={false}>
     <a className={styles.gold_item}>
-      <div className={styles.countLabel}>{item.collectionCount}</div>
-      <div>
+      <div className={styles.badge}>
+        <Icon type="caret-up" />
+        <p className={styles.badge_count}>{item.collectionCount}</p>
+      </div>
+      <div className={styles.content}>
         <div className={styles.title}>{item.title}</div>
-        <div className={styles.subTetle}>
+        <div className={styles.subtitle}>
           <div className={styles.time}>{moment(item.date).fromNow()}</div>
           <div className={styles.username}>{item.user.username}</div>
         </div>
