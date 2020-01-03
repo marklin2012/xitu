8ac8d739f46e2bdaaced352471a962e79a7dddfd
marklin2012 <marklin2012@sina.cn>
2020-01-03 23:46:26 +0800

    feat: 初始数据加载

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,29 @@ import React from 'react'
 import Home from '../components/Home'
 import Page from './_app'
 import { fetchGoldList } from '../reducer/gold'
+
 import { connect } from 'react-redux'
+import { fetchGithubList } from '../reducer/github'
 
 const Index = () => (
   <Home />
 )
 
 Index.getInitialProps = async (ctx) => {
-  return ctx.store.dispatch(fetchGoldList({
+  await ctx.store.dispatch(fetchGoldList({
     category: "frontend",
     order: "time",
     offset: 0,
     limit: 30
   }))
+  await ctx.store.dispatch(fetchGithubList({
+    category: "trending",
+    period: "day",
+    lang: "javascript",
+    offset: 0,
+    limit: 30
+  }))
+  return
 }
 
 export default connect(null, { fetchGoldList })(Index) 
